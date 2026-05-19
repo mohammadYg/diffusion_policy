437d5c3332e3fae19e17fc0e0e4b8038c9649226
Mohammad Hasan Yeganegi <yeganegi.m94@gmail.com>
2026-05-19 14:05:09 +0200

    modify the initialization of posterior

--- diffusion_policy/model/diffusion/bayes_conv1d_components.py
+++ diffusion_policy/model/diffusion/bayes_conv1d_components.py
@@ -225,7 +225,10 @@ class ProbLinear(nn.Module):
         if init_post == 'zeros':
             weights_mu_init = torch.zeros(out_features, in_features)
         elif init_post == 'random':
-            weights_mu_init = trunc_normal_(torch.Tensor(out_features, in_features), 0, sigma_weights, -2*sigma_weights, 2*sigma_weights)
+            if init_prior == 'random':
+                weights_mu_init = weights_mu_prior.clone()
+            else:
+                weights_mu_init = trunc_normal_(torch.Tensor(out_features, in_features), 0, sigma_weights, -2*sigma_weights, 2*sigma_weights)
         else:
             raise RuntimeError(f'Wrong posterior initialization. It should be either "zeros" or "random", but got {init_post}')
 
@@ -360,8 +363,11 @@ class ProbConv1d(nn.Module):
         if init_post == 'zeros':
             weights_mu_init = torch.zeros(out_channels, in_channels, kernel_size)
         elif init_post == 'random':
-            weights_mu_init = trunc_normal_(torch.Tensor(out_features, in_features, kernel_size),
-                                    0, sigma_weights, -2*sigma_weights, 2*sigma_weights)   
+            if init_prior == 'random':
+                weights_mu_init = weights_mu_prior.clone()
+            else:
+                weights_mu_init = trunc_normal_(torch.Tensor(out_features, in_features, kernel_size),
+                                        0, sigma_weights, -2*sigma_weights, 2*sigma_weights)   
         else:
             raise RuntimeError(f'Wrong posterior initialization. It should be either "zeros" or "random", but got {init_post}')
         
@@ -491,8 +497,11 @@ class ProbConvTranspose1d(nn.Module):
         if init_post == 'zeros':
             weights_mu_init = torch.zeros(in_channels, out_channels, kernel_size)
         elif init_post == 'random':
-            weights_mu_init = trunc_normal_(torch.Tensor(
-                in_channels, out_channels, kernel_size), 0, sigma_weights, -2*sigma_weights, 2*sigma_weights)
+            if init_prior == 'random':
+                weights_mu_init = weights_mu_prior.clone()
+            else:
+                weights_mu_init = trunc_normal_(torch.Tensor(
+                    in_channels, out_channels, kernel_size), 0, sigma_weights, -2*sigma_weights, 2*sigma_weights)
         else:
             raise RuntimeError(f'Wrong posterior initialization. It should be either "zeros" or "random", but got {init_post}')
 
